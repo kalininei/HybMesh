23e0a69662775df4e5050e936518835c9ad98da3
kalininei <kalininei@yandex.ru>
2016-02-29 23:47:06 +0300

    0.2.1

--- testing/py/bgrid_test.py
+++ testing/py/bgrid_test.py
@@ -113,7 +113,7 @@ hm.export_grid_vtk(g6, "_g6.vtk")
 hm.export_contour_vtk(g6, "_c6.vtk")
 check_grid(g6, 11280, 20304, 9024, {4: 9024})
 check_cont(g6, 4512, 4512, [400, 368, 1872, 1872], {0: 2240, 1: 400, 2: 1872})
-exit()
+
 
 print "doubly connected area: only single contour"
 op.start_point = op.end_point = [0, 0]
