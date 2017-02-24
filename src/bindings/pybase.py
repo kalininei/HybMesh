c82695499dddc91a949b3f5ffcc65b3130ffa56b
ek <ek@linux-xykk.site>
2017-02-24 18:13:10 +0300

    mask parser for python23 bindings

---
+++ src/bindings/pybase.py
@@ -0,0 +1,28 @@
+class Hybmesh(object):
+    def __init__(self):
+        super(Hybmesh, self).__init__()
+
+
+    class Contour2D(object):
+        def __init__(self):
+            super(Contour2D, self).__init__()
+        #>>$Contour2D
+
+
+    class Grid2D(object):
+        def __init__(self):
+            super(Grid2D, self).__init__()
+        #>>$Grid2D
+
+
+    class Surface3D(object):
+        def __init__(self):
+            super(Surface3D, self).__init__()
+        #>>$Surface3D
+
+
+    class Grid3D(object):
+        def __init__(self):
+            super(Grid3D, self).__init__()
+        #>>$Grid3D
+    #>>$Hybmesh
